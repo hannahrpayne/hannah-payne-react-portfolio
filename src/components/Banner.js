576baa0dbdb18567ebce26ff34b3d34f9d50e8f0
hannahrpayne <hannahrosepayne99@gmail.com>
2023-11-06 11:17:15 -0700

    added skills section and more styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import React from "react"
 import {Col, Container, Row} from "react-bootstrap";
 import { ArrowRightCircle} from "react-bootstrap-icons";
-import headerImg from '../assets/img/banner-bg.png';
+import headerImg from '../assets/img/header-img.svg';
 
 export const Banner = () => {
 
@@ -37,9 +37,9 @@ export const Banner = () => {
             setDelta(period);
             
         } else if (isDeleting  && updatedText === "") {
-            setIsDeleting(loopNum + 1);
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
             setDelta(500);
-            
         }
 
 
@@ -51,9 +51,9 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7} >
-                        <span className="tagline">Hannah Payne Portfolio</span>
-                        <h1><span>{`Hi I'm a Front End Developer`}</span></h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hello I'm a `}<span className="txt-rotate"><span className="wrap">{text}</span></span></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut  </p>
                         <button onClick={()=>console.log('connect')}>Let's connect<ArrowRightCircle size={25} /></button>
                     </Col>
                     <Col xs={12} md={6} xl={5} >
