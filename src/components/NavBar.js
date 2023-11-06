d66685ef4b08a4cb391efabbf9c8dd9d876af84f
hannahrpayne <hannahrosepayne99@gmail.com>
2023-11-06 09:40:18 -0700

    banner added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
@@ -28,13 +28,13 @@ export const NavBar = () => {
     }
 
     return (
-        <Navbar expand="md" className={scrolled ? "scrolled": ""}>
+        <Navbar expand="md" className= {scrolled ? "scrolled": ""}>
           <Container>
-            <Navbar.Brand href="/">
+            <Navbar.Brand href="home">
                 <img src={logo} alt="Logo" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
-                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon"></span> 
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
@@ -55,3 +55,6 @@ export const NavBar = () => {
         </Navbar>
       );
 };
+
+
+
