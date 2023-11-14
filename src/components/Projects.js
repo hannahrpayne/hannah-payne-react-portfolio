f3c07d9d7da12328cce68551d9116b9638057123
hannahrpayne <hannahrosepayne99@gmail.com>
2023-11-13 19:22:47 -0700

    This commit is a mistake

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,1 +1,50 @@
-export const 
+import {Col, Container, Row} from "react-bootstrap";
+
+
+export const Projects = () => {
+
+    const projects = [
+        {
+            title: "Artist Website Portfolio" ,
+            description: "Portfolio that I created and designed for a client who is an artist.",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Artist Website Portfolio" ,
+            description: "Portfolio that I created and designed for a client who is an artist.",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Artist Website Portfolio" ,
+            description: "Portfolio that I created and designed for a client who is an artist.",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Artist Website Portfolio" ,
+            description: "Portfolio that I created and designed for a client who is an artist.",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Artist Website Portfolio" ,
+            description: "Portfolio that I created and designed for a client who is an artist.",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Artist Website Portfolio" ,
+            description: "Portfolio that I created and designed for a client who is an artist.",
+            imgUrl: projImg1,
+        }
+    ]
+
+
+    return (
+        <section className="Project" id="Project">
+            <Container>
+                <Row>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
