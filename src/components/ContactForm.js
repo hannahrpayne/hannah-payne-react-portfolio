3dbcdcd2bc4992efe78030805b37437b770cc0ab
hannahrpayne <hannahrosepayne99@gmail.com>
2024-02-19 17:56:45 -0700

    added netlify form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -52,27 +52,63 @@ export const ContactForm = () => {
             
             <Container>
                         <h2>Get In Touch</h2>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit} name="contact" data-netlify="true">
+                            <input
+                            type="hidden"
+                            name="form-name"
+                            value="contact"
+                             />
                         {/* <form> */}
                             <Row>
                                 <Col sm={6} className='px-1'>
                                     <input
                                     type="text" 
-                                    value={formDetails.firstName} 
+                                    id="first-name"
+                                    name="first-name"
+                                    // value={formDetails.firstName} 
                                     placeholder="First Name" 
-                                    onChange={(e) => onFormUpdate("firstName", e.target.value)} />
+                                    // onChange={(e) => onFormUpdate("firstName", e.target.value)} 
+                                    />
                                 </Col>
                                 <Col sm={6} className='px-1'>
-                                <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate("lastName", e.target.value)} />
+                                    <input 
+                                    type="text" 
+                                    id="last-name"
+                                    name="last-name"
+                                    // value={formDetails.lastName} 
+                                    placeholder="Last Name" 
+                                    // onChange={(e) => onFormUpdate("lastName", e.target.value)} 
+                                    />
                                 </Col>
                                 <Col sm={6} className='px-1'>
-                                <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate("email", e.target.value)} />
+                                    <input 
+                                    type="email" 
+                                    id="email"
+                                    name="email"
+                                    // value={formDetails.email} 
+                                    placeholder="Email" 
+                                    // onChange={(e) => onFormUpdate("email", e.target.value)} 
+                                    />
                                 </Col>
                                 <Col sm={6} className='px-1'>
-                                <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e) => onFormUpdate("phone", e.target.value)} />
+                                    <input 
+                                    type="tel" 
+                                    id="phone"
+                                    name="phone"
+                                    // value={formDetails.phone} 
+                                    placeholder="Phone Number" 
+                                    // onChange={(e) => onFormUpdate("phone", e.target.value)} 
+                                    />
                                 </Col>
                                 <Col>
-                                <textarea rows={6} value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate("message", e.target.value)} />
+                                    <textarea 
+                                    rows={6} 
+                                    name="message"
+                                    id="message"
+                                    // value={formDetails.message} 
+                                    placeholder="Message" 
+                                    // onChange={(e) => onFormUpdate("message", e.target.value)} 
+                                    />
                                 <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
 
